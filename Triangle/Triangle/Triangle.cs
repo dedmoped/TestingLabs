9d9f4ac98523ed015cb845847c09d307ff6d8c40
dedmoped <vilbicos2000@gmail.com>
2019-11-17 19:11:31 +0200

    add labs

---
+++ Triangle/Triangle/Triangle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangle
+{
+    public class Triangle
+    {
+        public bool CheckIsPossibleToCreate(int A_Side, int B_Side, int C_Side)
+        {
+
+
+            return (A_Side >= 0 && B_Side >= 0 && C_Side >= 0 && (A_Side + B_Side > C_Side && B_Side + C_Side > A_Side && A_Side + C_Side > B_Side));
+
+        }
+    }
+}
---
+++ Triangle/UnitTestProject1/TriangleTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Triangle;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class Ttriangle_Tests
+    {
+        Triangle.Triangle triangle = new Triangle.Triangle();
+        [TestMethod]
+        public void A_Is_Negative()
+        {
+            Assert.IsFalse(triangle.CheckIsPossibleToCreate(-4,6,7));
+        }
+        [TestMethod]
+        public void B_Is_Negative()
+        {
+            Assert.IsFalse(triangle.CheckIsPossibleToCreate(4, -6, 7));
+        }
+        [TestMethod]
+        public void C_Is_Negative()
+        {
+            Assert.IsFalse(triangle.CheckIsPossibleToCreate(4, 6, -7));
+        }
+        [TestMethod]
+        public void A_Is_less()
+        {
+            Assert.IsFalse(triangle.CheckIsPossibleToCreate(8, 16, 33));
+        }
+        [TestMethod]
+        public void B_Is_less()
+        {
+            Assert.IsFalse(triangle.CheckIsPossibleToCreate(16, 8, 33));
+        }
+        [TestMethod]
+        public void C_Is_less()
+        {
+            Assert.IsFalse(triangle.CheckIsPossibleToCreate(16, 33, 8));
+        }
+        [TestMethod]
+        public void A_Is_null()
+        {
+            Assert.IsFalse(triangle.CheckIsPossibleToCreate(0, 33, 8));
+        }
+        [TestMethod]
+        public void B_Is_null()
+        {
+            Assert.IsFalse(triangle.CheckIsPossibleToCreate(16, 0, 8));
+        }
+        [TestMethod]
+        public void C_Is_null()
+        {
+            Assert.IsFalse(triangle.CheckIsPossibleToCreate(16, 8, 0));
+        }
+        [TestMethod]
+        public void Egyptian_True()
+        {
+            Assert.IsTrue(triangle.CheckIsPossibleToCreate(3, 4, 5));
+        }
+    }
+}
